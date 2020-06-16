4287fff6d230bab330a822ff9bbdba48fa8cc967
Incaze <e.kalabkin@gmail.com>
2020-06-16 18:34:29 +0300

    Initial commit

---
+++ src/main/kotlin/com/incaze/seafight/SeafightApplication.kt
@@ -0,0 +1,11 @@
+package com.incaze.seafight
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeafightApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeafightApplication>(*args)
+}
---
+++ src/test/kotlin/com/incaze/seafight/SeafightApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.incaze.seafight
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeafightApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
